c9220b303db4834128ebaa47e7822500161aa9ec
yettd <junkai.poh@gmail.com>
2020-06-18 13:43:06 +0800

    updated by junkai

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    public Rigidbody rb;
+    private float maxY=3.6f;
+    private float minY = -5;
+    private float y;
+    private float score = 0;
+    public Text t;
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = transform.up * 250*Time.deltaTime;
+            
+
+        }
+        if(transform.position.y<-4)
+        {
+            PlayerPrefs.SetFloat("s", score);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    private void LateUpdate()
+    {
+        y = Mathf.Clamp(transform.position.y, minY, maxY);
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="wall")
+        {
+            PlayerPrefs.SetFloat("s", score);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "score")
+        {
+            score++;
+            t.text = "SCORE : " + score;
+        }
     }
 }
---
+++ Assets/gameover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class gameover : MonoBehaviour
+{
+    public Text text;
+    float score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = PlayerPrefs.GetFloat("s");
+        text.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
